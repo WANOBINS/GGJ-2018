441580c7fd2c9f59f0369edc2e3a252bed1c0b3d
DaemonUmbra <daemon.umbra@gmail.com>
2018-01-26 20:48:00 -0500

    Added SteamVR, VRInputManager, and enabled git lfs
    git lfs will take the large files and handle them more efficiently

---
+++ Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    internal class Singleton : MonoBehaviour
+    {
+        #region Variables
+
+        //List of types of which there can be one and only one in each scene
+        private List<Type> singletons = new List<Type>();
+
+        #endregion Variables
+
+        #region Unity Functions
+
+        private void Start()
+        {
+            //Get Types from project
+            List<Type> classes = new List<Type>(System.Reflection.Assembly.GetExecutingAssembly().GetTypes());
+
+            //Loop through them
+            foreach (Type type in classes)
+            {
+                //and get their attributes
+                List<object> attributes = new List<object>(type.GetCustomAttributes(false));
+
+                //If they have the OneInScene attribute
+                if (attributes.Contains(typeof(OneInSceneAttribute)))
+                {
+                    //Add it to the singleton list
+                    singletons.Add(type);
+                }
+            }
+
+            //Now go through the singletons
+            foreach (Type type in singletons)
+            {
+                List<GameObject> objects = ManualConvertToList(FindObjectsOfType(type));
+                if (objects.Count > 1)
+                {
+                    throw new Exception("There must be only " + type.Name + " in the scene at any time");
+                }
+            }
+        }
+
+        #endregion Unity Functions
+
+        #region Helper Functions
+
+        private List<GameObject> ManualConvertToList(UnityEngine.Object[] @object)
+        {
+            List<GameObject> output = new List<GameObject>();
+            foreach (UnityEngine.Object miniobject in @object)
+            {
+                output.Add((GameObject)miniobject);
+            }
+            return output;
+        }
+
+        #endregion Helper Functions
+    }
+}
