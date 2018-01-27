da374cdc134ee847a0b58a56e1fd40bacf2fd3cc
Steve <thesteveoninja@gmail.com>
2018-01-27 00:53:45 -0500

    Began Work on StevePlayerController
    NOTE: Steam VR has it's own player controller and tried to fight me. This was the compromise

---
+++ Assets/Scripts/StevePlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StevePlayerController : MonoBehaviour {
+
+    #region Variables
+
+    public GameObject L_Hand;
+    public GameObject R_Hand;
+
+    #endregion Variables
+
+    // Use this for initialization
+    void Start () {
+        L_Hand = GameObject.FindGameObjectWithTag("Left");
+        R_Hand = GameObject.FindGameObjectWithTag("Right");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
