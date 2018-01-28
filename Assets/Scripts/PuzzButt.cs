9d3128dcee6b2d3e2d5ae122ad64d4e2c9d12549
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-01-28 13:59:53 -0500

    fixed button name conflict

--- Assets/Scripts/PuzzButt.cs
+++ Assets/Scripts/PuzzButt.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour {
+public class PuzzButt : MonoBehaviour {
     public PuzzleSequence PSq;
     
 	// Use this for initialization
