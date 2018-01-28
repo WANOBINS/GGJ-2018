fb5f8f2ad40a34ec02bb9e70601707efe2d139fc
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-01-28 12:21:00 -0500

    Added PuzzleSequence.cs

---
+++ Assets/Scripts/PuzzleSequence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSequence : MonoBehaviour
+{
+
+    public bool teleporterPower = false;
+    public ParticleSystem teleparticle;
+    public GameObject LGButt;
+    public GameObject SGButt;
+    public GameObject LGSwitch;
+    public GameObject SGSwitch;
+    public GameObject GCrystal;
+
+    public GameObject LRButt; // FIRE Button
+    public GameObject SRButt;
+    public GameObject LRSwitch;
+    public GameObject SRSwitch;
+    public GameObject RCrystal;
+
+    public GameObject LBButt;
+    public GameObject SBButt;
+    public GameObject LBSwitch;
+    public GameObject SBSwitch;
+    public GameObject BCrystal;
+
+    public GameObject[] MySequence;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GameObject[] CorrectSequence = { LGButt, SBSwitch, RCrystal, SBButt };
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider Hand)
+    {
+        
+    }
+}
