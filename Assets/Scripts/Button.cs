3be95bab166f449c87af8b9d14990d9277964c67
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-01-28 13:45:30 -0500

    Finnished PuzzleSequens, more or less

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Button : MonoBehaviour {
-
+    public PuzzleSequence PSq;
+    
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        PSq = FindObjectOfType<PuzzleSequence>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    private void OnTriggerEnter(Collider Hand)
+    {
+        if (Hand.tag == "Left" || Hand.tag == "Right")
+        {
+            PSq.MySequence.Add(this.gameObject);
+
+            
+        }
+    }
 }
--- Assets/Scripts/PuzzleSequence.cs
+++ Assets/Scripts/PuzzleSequence.cs
@@ -7,47 +7,58 @@ public class PuzzleSequence : MonoBehaviour
 
     public bool teleporterPower = false;
     public ParticleSystem teleparticle;
-    public GameObject LGButt;
-    public GameObject SGButt;
-    public GameObject LGSwitch;
-    public GameObject SGSwitch;
-    public GameObject GCrystal;
+    public ParticleSystem earthExplode;
 
-    public GameObject LRButt; // FIRE Button
-    public GameObject SRButt;
-    public GameObject LRSwitch;
-    public GameObject SRSwitch;
-    public GameObject RCrystal;
 
-    public GameObject LBButt;
-    public GameObject SBButt;
-    public GameObject LBSwitch;
-    public GameObject SBSwitch;
-    public GameObject BCrystal;
+    public  GameObject LGButt;
+    public  GameObject SGButt;
+    public  GameObject LGSwitch;
+    public  GameObject SGSwitch;
+    public  GameObject GCrystal;
 
-    public GameObject[] MySequence;
+    public  GameObject LRButt; // FIRE Button
+    public  GameObject SRButt;
+    public  GameObject LRSwitch;
+    public  GameObject SRSwitch;
+    public  GameObject RCrystal;
+
+    public  GameObject LBButt;
+    public  GameObject SBButt;
+    public  GameObject LBSwitch;
+    public  GameObject SBSwitch;
+    public  GameObject BCrystal;
+
+    // public GameObject[] MySequence;
+    public List<GameObject> MySequence;
+    public List<GameObject> CorrectSequence;
+    
 
     // Use this for initialization
     void Start ()
     {
-        GameObject[] CorrectSequence = { LGButt, SBSwitch, RCrystal, SBButt };
-        
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    { 
-		//SBButt.GetComponent<Collider>
-	}
-
-   /* public PuzzleSequence PSq;
-    PSq = FindObjectOfType<PuzzleSequence>();*/
-
-    private void OnTriggerEnter(Collider Hand)
-    {
-        if(Hand.tag == "Left"|| Hand.tag == "Right")
-        {
-            
-        }
+        CorrectSequence.Add(LGButt);
+        CorrectSequence.Add(SBSwitch);
+        CorrectSequence.Add(RCrystal);
+        CorrectSequence.Add(SBButt);
+        CorrectSequence.Add(LRButt);
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SBButt.GetComponent<Collider>
+        if (MySequence[MySequence.Count - 1] == LRButt)
+        {
+            if(MySequence == CorrectSequence)
+            {
+                teleporterPower = true;
+                //teleparticle.emission.enabled
+                //enable emission
+            }
+            else
+            {
+                //Blow up earth
+            }
+        }        
+	}    
 }
